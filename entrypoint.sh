95f8e031f808301dd135cc94c12c988aabf5c3b7
Zhenguo Zhang <zhangz.sci@gmail.com>
2022-06-13 12:20:41 -0700

    use entrypoint.sh in repo

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -l
+
+echo "Get new version based on current version in versions.json"
+zone=dockerImage
+verPos=file
+verFile=versions.json
+newVersion=$(bump_versions \
+    --zone=$zone \
+    --version-pos=$verPos \
+    $verFile)
+
+echo "::set-output name=newVersion::$newVersion"
+
